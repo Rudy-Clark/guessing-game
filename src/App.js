93de355b41b0cc80ccfb386ecfb4ada0b6c012cb
Rudy Clark <rudy.uzb@gmail.com>
2019-05-02 19:55:46 +0500

    environment configured

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import themeProvider from './themeProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const style = () => ({
+  container: {
+    maxWidth: '360px',
+    margin: '0 auto',
+  },
+});
 
-export default App;
+const App = ({ classes }) => (
+  <div className={classes.container}>
+    <h1>Hello World</h1>
+  </div>
+);
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default themeProvider(withStyles(style)(App));
