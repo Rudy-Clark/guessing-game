1941f6e9802da32d2033802f042abf4216b22596
Rudy Clark <rudy.uzb@gmail.com>
2019-05-06 09:04:44 +0500

    add Overlay component to hide Cell

---
+++ src/components/Overlay.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { choiceItem } from '../actions';
+
+const styles = () => ({
+  overlay: {
+    minWidth: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    outline: 'none',
+    backgroundColor: '#fff',
+    border: '1px solid #e3e3e3',
+    position: 'absolute',
+    zIndex: 999,
+    cursor: 'pointer',
+  },
+});
+
+const Overlay = ({ id, color, handleChoice, classes }) => (
+  <div
+    role="button"
+    onClick={e => {
+      e.stopPropagation();
+      handleChoice(color, id);
+    }}
+    onKeyDown={() => {}}
+    tabIndex={-1}
+    className={classes.overlay}
+  />
+);
+
+Overlay.propTypes = {
+  classes: PropTypes.object.isRequired,
+  color: PropTypes.string.isRequired,
+  handleChoice: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+const styledComponent = withStyles(styles)(Overlay);
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = dispatch => ({
+  handleChoice: (color, id) => dispatch(choiceItem(color, id)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(styledComponent);
